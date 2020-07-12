a49be9ce94855c9ffd4e2a29a0db05bb7879d29e
mixth <me@mixth.net>
2020-07-12 10:56:19 +0700

    Add test case to demonstrate first-class function pulling

---
+++ main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCheckInHandler(t *testing.T) {
+	payload := new(bytes.Buffer)
+	json.NewEncoder(payload).Encode(&Check{ID: 123, PlaceID: 234})
+	req := httptest.NewRequest("POST", "http://example.com/foo", payload)
+	w := httptest.NewRecorder()
+
+	origin := InsertCheckIn
+	defer func() {
+		InsertCheckIn = origin
+	}()
+	InsertCheckIn = func(ID, placeID int64) error {
+		return nil
+	}
+
+	CheckIn(w, req)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	fmt.Println(resp.StatusCode)
+	fmt.Println(resp.Header.Get("Content-Type"))
+	fmt.Println(string(body))
+}
